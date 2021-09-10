8b694086694946d56695c39ca1d5cb3d186fb475
educartj <educartj@gmail.com>
2021-09-10 19:23:47 -0300

    Database docker

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,21 +1,10 @@
 import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 
-export default async (host = 'database_cartanoel'): Promise<Connection> => {
+export default async (): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
   // pelo host
-  // return createConnection(
-  //   Object.assign(defaultOptions, {
-  //     database:
-  //       process.env.NODE_ENV === 'test'
-  //         ? 'api_test'
-  //         : defaultOptions.database,
-  //   }),
-  // );
-  // pelo docker
   return createConnection(
     Object.assign(defaultOptions, {
-      host: process.env.NODE_ENV === 'test' ? 'localhost' : host,
-
       database:
         process.env.NODE_ENV === 'test' ? 'api_test' : defaultOptions.database,
     }),
